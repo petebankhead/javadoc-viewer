1681e19dde2eb30cc8d5adac5b374fe05f4f6071
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2024-05-07 11:52:51 +0100

    Typo

--- javadocviewer-main/src/main/java/qupath/ui/javadocviewer/package-info.java
+++ javadocviewer-main/src/main/java/qupath/ui/javadocviewer/package-info.java
@@ -2,11 +2,11 @@
  * This package contains the Javadoc viewer.
  * <ul>
  *     <li>
- *         The {@link qupath.ui.javadocviewer.main.core} package contains utilities
+ *         The {@link qupath.ui.javadocviewer.core} package contains utilities
  *         to search for Javadocs.
  *     </li>
  *     <li>
- *         The {@link qupath.ui.javadocviewer.main.gui} package contains the UI elements
+ *         The {@link qupath.ui.javadocviewer.gui} package contains the UI elements
  *         of the application.
  *     </li>
  * </ul>
