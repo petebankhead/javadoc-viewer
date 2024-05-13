4e8d11819e846a078184d75373e6bdf451dcba75
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2024-05-13 12:45:11 +0100

    Unused import

--- javadocviewer/src/main/java/qupath/ui/javadocviewer/core/JavadocsFinder.java
+++ javadocviewer/src/main/java/qupath/ui/javadocviewer/core/JavadocsFinder.java
@@ -12,7 +12,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
